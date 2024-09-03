696f542d2aa4339466f2173aa6a5274334904035
abijo-rajesh <abijorajesh@gmail.com>
2024-09-03 12:20:37 +0530

    back base code

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const commonMiddleware = require('./middleware/commonMid')
 dotenv.config();
 const PORT = process.env.PORT;
 const newUserRoute = require('./routes/newUser')
+const newVedor= require('./routes/vendor')
 const connectDB=require('./db')
 
 commonMiddleware(app);
 
 // app.use('/user', require('./routes/userauth'))
 app.use('/newUser', newUserRoute);
+app.use('/vendor', newVedor);
 
 connectDB()
 
