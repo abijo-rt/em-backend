c907793e9ca7c491bde4849c8d82691c611c8ce4
abijo-rt <abijorajesh@gmail.com>
2024-10-04 19:00:52 +0530

    deploy

--- Model/vendorDetails.js
+++ Model/vendorDetails.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const venderdetails = new mongoose.Schema({
     vendor_email :String,
--- controller/vendor/loginVendor.js
+++ controller/vendor/loginVendor.js
@@ -1,5 +1,5 @@
 const VendorDetails =require('../../Model/vendorDetails')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const vendorLogin = async (req , res) => {
