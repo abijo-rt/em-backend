c5734c4838fe30fe4cdeb64dec7d830128c9a42e
abijo-rt <abijorajesh@gmail.com>
2024-10-04 18:46:56 +0530

    deploy

--- index.js
+++ index.js
@@ -11,33 +11,6 @@ const connectDB = require('./db')
 
 
 
-// const upload = require('./multer')
-
-// app.post('/vendor/upload', (req, res, next) => {
-//   console.log('Request Headers:', req.headers);
-//   next();
-// },
-//   upload.single('file'), (req, res) => {
-//     try
-//     {
-//       console.log('Uploaded File Details:', req.file);  // Log the uploaded file details
-
-//       if (!req.file)
-//       {
-//         return res.status(400).send('No file uploaded.');
-//       }
-
-//       res.send({
-//         message: 'File uploaded successfully',
-//         file: req.file,
-//       });
-//     } catch (error)
-//     {
-//       console.error('File upload error:', error);
-//       res.status(500).send('Internal server error during file upload.');
-//     }
-//   });
-
 
 // commonMiddleware(app);
 app.use('/user', userRoute);
